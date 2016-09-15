a6cb810c916ed6d11695e71be09b4499f31ce44d
Toni Miharja <168tonimiharja@gmail.com>
2016-09-16 00:41:17 +0800

    Add isMutating to Command

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,7 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws exception;
 
     /**
      * Supplies the data the command will operate on.
@@ -65,4 +65,5 @@ public abstract class Command {
     public void setTargetIndex(int targetIndex) {
         this.targetIndex = targetIndex;
     }
+    public abstract boolean isMutating();
 }
