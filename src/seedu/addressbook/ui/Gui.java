28e59ad827938908eecac136a4236758de5e888b
Toni Miharja <168tonimiharja@gmail.com>
2016-09-16 00:58:02 +0800

    Add icon

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
+import javafx.scene.image.Image;
 
 import java.io.File;
 import java.io.IOException;
@@ -41,6 +42,7 @@ public class Gui {
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
         stage.show();
         MainWindow mainWindow = loader.getController();
+        stage.getIcons().add(new Image("file:src/seedu/addressbook/ui/symbol.png"));
         mainWindow.setLogic(logic);
         mainWindow.setMainApp(mainApp);
         return mainWindow;
